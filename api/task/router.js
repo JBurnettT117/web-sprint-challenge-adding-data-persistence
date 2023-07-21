8663541406f48c9496c8b894c6d173965093861e
Josh Burnett <joshuaburnett429@gmail.com>
2023-07-21 15:56:51 -0500

    router files functioning

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,14 @@
 // build your `/api/tasks` router here
+const router = require('express').Router();
+
+const Task = require('./model')
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: "something went wrong inside the Task router",
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
